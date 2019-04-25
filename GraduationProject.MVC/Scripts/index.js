0519838d24da3a7c87b2c3759e53545c3c36bdfe
Sayed Mahmoud Sayed <sayedmahmoud266@yahoo.com>
2019-04-25 16:54:10 +0200

    add tansiq to (faculty && division) design

--- GraduationProject.MVC/Scripts/index.js
+++ GraduationProject.MVC/Scripts/index.js
@@ -63,7 +63,28 @@
             let newGovtOption = $(`<option ${selected}>${govt}</option>`);
             $eltGovernorateSelector.append(newGovtOption);
         }
+
+        const $tansiqSelectionComponent = $('tansiq');
+        if ($tansiqSelectionComponent) {
+            if ($tansiqSelectionComponent.attr('for') === 'division') {
+                console.log('its for division');
+            }
+            console.log('tansiq exists');
+            $tansiqSelectionComponent.append($(TansiqCreationComponentTemplate()));
+        }
     }
+
+    $('input[tansiqActivationBox]').change(event => {
+        let checked = event.target.checked;
+        let $target = $(event.target);
+        let $containerWrapper = $target.parent().parent().find('tansiqInfoContainer');
+        let type = $target.attr('colleger-tansiqType');
+        if (checked) {
+            $containerWrapper.append($(TansiqCreationInfoTemplate(type)));
+        } else {
+            $containerWrapper.empty();
+        }
+    });
 
 
     window.imageFileCheck = function (obj) {
@@ -74,3 +95,42 @@
         }
     };
 })();
+
+function TansiqCreationComponentTemplate(forDivision = false) {
+    let tansiqTemplate = `<div class="col w-100">`;
+    let arrayOfSpecializations = ['3lmy 3loom', '3lmyryada', 'adaby'];
+    for (const i of arrayOfSpecializations) {
+        let specializationObjectTemplate = `<div>
+            <div class="form-check">
+              <input class="form-check-input" tansiqActivationBox colleger-tansiqType="${i}" type="checkbox" value="" id="tansiqCheck-${i}">
+              <label class="form-check-label" for="tansiqCheck-${i}">
+                Accepts ${i}
+              </label>
+            </div>
+            <tansiqInfoContainer></tansiqInfoContainer>
+        </div>`;
+        tansiqTemplate += specializationObjectTemplate;
+    }
+    tansiqTemplate += `</div>`;
+    return tansiqTemplate;
+}
+
+function TansiqCreationInfoTemplate(forType, isEdit = false) {
+    let tansiqInfoTemplate = `<div class="row">
+    <div class="col-auto">
+        <label class="sr-only" for="tansiq['${forType}'].from">From</label>
+        <input type="number" class="form-control mb-2" id="tansiq['${forType}'].from" placeholder="From" name="tansiq['${forType}'].from">
+    </div>
+    <div class="col-auto">
+        <label class="sr-only" for="tansiq['${forType}'].to">To</label>
+        <input type="number" class="form-control mb-2" id="tansiq['${forType}'].to" placeholder="To" name="tansiq['${forType}'].to">
+    </div>
+    ${isEdit ? `
+    <div class="col-auto">
+        <label class="sr-only" for="tansiq['${forType}'].actual">Actual</label>
+        <input type="number" class="form-control mb-2" id="tansiq['${forType}'].actual" placeholder="Actual" name="tansiq['${forType}'].actual">
+    </div>
+`: ''}
+    </div>`;
+    return tansiqInfoTemplate;
+}
