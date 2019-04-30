608febc972328af932c2d4fcef542040bed34a3c
Sayed Mahmoud Sayed <sayedmahmoud266@yahoo.com>
2019-04-30 18:35:05 +0200

    dashboard index

--- GraduationProject.MVC/Controllers/DashboardController.cs
+++ GraduationProject.MVC/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GraduationProject.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,31 @@ namespace GraduationProject.MVC.Controllers
 {
     public class DashboardController : Controller
     {
+        private DatabaseContext db = new DatabaseContext();
+
         // GET: Dashboard
         public ActionResult Index()
         {
+            
+            ViewBag.usersCount = db.Students.Count();
+            ViewBag.universitiesCount = db.Universities.Count();
+            ViewBag.searchCount = db.SearchHistories.Count();
+            ViewBag.savedRecommendations = db.Recommendations.Count(r => r.UserId != null);
+
+            ViewBag.countPerDayLabels = db.SearchHistories
+                .GroupBy(x => DbFunctions.TruncateTime(x.Timestamp))
+                .Select(x => new
+                {
+                    Day = (DateTime)x.Key
+                }).ToList();
+            ViewBag.countPerDayData = db.SearchHistories
+                .GroupBy(x => DbFunctions.TruncateTime(x.Timestamp))
+                .Select(x => new
+                {
+                        Value = x.Count()
+                }).ToList();
+
+
             return View();
         }
     }
